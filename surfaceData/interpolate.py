4a5426c7bf8318a8e16f4feb3a67a0f53b1f33bd
Aidan Bharath <aidanbharath@gmail.com>
2015-01-16 17:13:16 +1100

    interpolation done

--- surfaceData/interpolate.py
+++ surfaceData/interpolate.py
@@ -1,13 +1,45 @@
 import numpy as np
 import pandas as pd
-import numba as nb
-from multiprocessing import pool
 
-def dataExtract()
 
-def linearInt(data,np=8);
+def dataDictTest(data):
 
-    pool = Pool(np)
-    keys = data.keys()
+    if type({}) != type(data):
+        data = {'data':data}
+    else:
+        pass
+
+    return data
+
+def coreInt(data,step):
+
+    tmax = data[:,0].max()
+    timeStep = np.linspace(0,tmax,tmax*step)
+    intArray = np.zeros([timeStep.shape[0],data[0,:].shape[0]])
+
+    for i in xrange(data[0,:].shape[0]-1):
+        i = i+1
+        intArray[:,i] = np.interp(timeStep,data[:,0],data[:,i])
+
+    intArray[:,0] = timeStep
+
+    return intArray
+
+
+def linearInt(data,step=100):
+
+    data = dataDictTest(data)
+    intD = {}
+    for keys in data:
+        colNames = data[str(keys)].columns.values.tolist()
+        intData = coreInt(data[str(keys)].values,step)
+        intD[str(keys)] = pd.DataFrame(intData,columns=colNames)
+
+    return intD
 
 if __name__ == "__main__":
+
+    import load
+    dirs = '/media/aidan/Seagate Expansion Drive/openFoam/convDatFiles'
+    data = load.loadSurfFile(dirs)
+    print linearInt(data)
--- surfaceData/load.py
+++ surfaceData/load.py
@@ -1,6 +1,5 @@
 from os import path,chdir,walk
 import glob
-import numpy as np
 import pandas as pd
 
 def findFiles(parDir):
@@ -17,11 +16,11 @@ def findFiles(parDir):
             try:
                 root,dirs,files = walk(str(paths))
                 data = path.join(files[0],files[-1][0])
+                dataDirs.append(pd.read_csv(data,delim_whitespace=True)[3::])
+                topPath.append(paths)
             except ValueError:
-                print 'Data doesnt exsist in '+str(paths)
+                print 'Data doesnt exist in '+str(paths)
                 pass
-        dataDirs.append(pd.read_csv(data,delim_whitespace=True))
-        topPath.append(paths)
 
     chdir(curDir)
     return dataDirs,topPath
@@ -38,4 +37,3 @@ if __name__ == "__main__":
     direct = '/media/aidan/Seagate Expansion Drive/openFoam/convDatFiles/'
     files = loadSurfFile(direct)
 
-    print files['dat1']
