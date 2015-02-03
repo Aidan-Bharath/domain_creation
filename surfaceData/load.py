5a26577622e8ecd15953d1ef96c1e33c2a6eb562
Aidan Bharath <Aidan.Bharath@utas.edu.au>
2015-02-03 16:31:01 +1100

    added StarCCM functionality

--- surfaceData/load.py
+++ surfaceData/load.py
@@ -1,8 +1,54 @@
 from os import path,chdir,walk
 import glob
+import numpy as np
 import pandas as pd
 
-def findFiles(parDir):
+def interpDF(df):
+    # Meant to specifically work on StarCCM data
+
+    height = np.zeros([len(df['Z (m)'])-1])
+
+    for i in xrange(height.shape[0]):
+        vof = df['Volume Fraction of Water'][i]
+        h = df['Z (m)'][i+1]-df['Z (m)'][i]
+        height[i] = vof*h
+
+    surface = height.sum()    
+    
+    return surface
+    
+
+def StarFindFiles(parDir):
+    curDir = path.abspath('./')
+    
+    # Unfortunitely StarCCM doesnt interpolate data so we must do that.
+
+    chdir(parDir)
+    allConts = glob.glob('*')
+    isDir = [path.isdir(dirs) for dirs in allConts]
+    
+    dataDirs = {}
+    for paths,tf in zip(allConts,isDir):
+        if tf == True:
+                dataPath = path.join(parDir,paths)
+                chdir(dataPath)
+
+                #Watch out here the sorting is not correct
+                files = sorted(glob.glob('*'))
+                dataFiles = [path.join(dataPath,fName) for fName in files]
+                sumArray = np.zeros([len(dataFiles)])                
+            
+                for index,data in enumerate(dataFiles):
+                    sumArray[index] = interpDF(pd.read_csv(data))
+                
+                
+                dataDirs[paths] = pd.DataFrame(sumArray)
+               
+    chdir(curDir)
+    
+    return dataDirs
+
+def OFFindFiles(parDir):
     curDir = path.abspath('./')
 
     chdir(parDir)
@@ -25,15 +71,19 @@ def findFiles(parDir):
     chdir(curDir)
     return dataDirs,topPath
 
-def loadSurfFile(parDir):
+def loadSurfFile(parDir,simType='OF'):
 
-    fileLists,paths = findFiles(parDir)
-    dataFiles = {path:fileList for path,fileList in zip(paths,fileLists)}
+    if simType == 'OF':
+        fileLists,paths = OFFindFiles(parDir)
+        dataFiles = {path:fileList for path,fileList in zip(paths,fileLists)}
+        
+    elif simType == 'StarCCM':
+        dataFiles = StarFindFiles(parDir)
 
     return dataFiles
 
 if __name__ == "__main__":
 
-    direct = '/media/aidan/Seagate Expansion Drive/openFoam/convDatFiles/'
-    files = loadSurfFile(direct)
+    direct = 'C://Users/ABHARATH/Documents/StarCCM/waveTank_4040/'
+    files = loadSurfFile(direct,simType='StarCCM')
 
