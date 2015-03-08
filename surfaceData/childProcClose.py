aee9e4af41a8484e6b64e1d0b548863eeae5d9ba
Aidan Bharath <Aidan.Bharath@utas.edu.au>
2015-03-08 17:43:27 +1100

    child processor fix

---
+++ surfaceData/childProcClose.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 08 17:40:39 2015
+
+@author: ABHARATH
+"""
+
+import psutil, os
+
+def killProcTree(pid, including_parent=False):    
+    pid = os.getpid()    
+    parent = psutil.Process(pid)
+    for child in parent.children(recursive=True):
+        child.kill()
+    if including_parent:
+        parent.kill()
+
